95fa888e29da7b870564dde856d07ff32e2677c9
Sy Le <lenguyensy@gmail.com>
2021-09-09 10:02:10 -0700

    Update .prettier Format config (#1)
    * code format
    
    * 1.0.1

--- src/UserApi.ts
+++ src/UserApi.ts
@@ -1,11 +1,11 @@
-import { makeRestApi, RestApiResponse } from "synle-rest-utils";
+import { makeRestApi, RestApiResponse } from 'synle-rest-utils';
 
 export interface UserAgentResponse {
-  "user-agent": string;
+  'user-agent': string;
 }
 
 export class UserApi {
   static getUserAgent(): RestApiResponse<UserAgentResponse> {
-    return makeRestApi("https://httpbin.org/user-agent");
+    return makeRestApi('https://httpbin.org/user-agent');
   }
 }
